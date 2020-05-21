5f4ddea5403b09d235a475266d423a553ba44453
Malincov <mmalincov@gmail.com>
2020-05-21 03:40:43 -0400

    Initial commit

---
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -0,0 +1,25 @@
+package com.example.instagramclone;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("mandat12-parstgram") // should correspond to APP_ID env variable
+                .clientKey("CodepathMoveFastParse")  // set explicitly unless clientKey is explicitly configured on Parse server
+               // .clientBuilder(builder)
+                .server("https://mandat12-parstgram.herokuapp.com/parse/").build());
+    }
+}
